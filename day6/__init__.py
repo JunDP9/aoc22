c7af4ba4d7834fca780862649406b9f5b4711b46
Jun Chatani <jun.chatani@persgroep.net>
2022-12-06 07:46:25 +0100

    Day 6: Refactor needed but meh

---
+++ day6/__init__.py
@@ -0,0 +1,40 @@
+from utils import parse_input
+
+FILE_PATH = 'input.txt'
+
+parsed_string = parse_input(FILE_PATH)
+
+
+def solution_part_one(parsed_string):
+    res = []
+    counter = 0
+    for char in parsed_string[0]:
+        if char in res:
+            while char in res:
+                res.pop(0)
+            res.append(char)
+
+        else:
+            res.append(char)
+        counter += 1
+        if len(res) > 3: return counter
+    return counter
+
+
+def solution_part_two(parsed_string):
+    res = []
+    counter = 0
+    for char in parsed_string[0]:
+        if char in res:
+            while char in res:
+                res.pop(0)
+            res.append(char)
+
+        else:
+            res.append(char)
+        counter += 1
+        if len(res) > 13: return counter
+    return counter
+
+print(solution_part_one(parsed_string))
+print(solution_part_two(parsed_string))
---
+++ day6/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from day3 import solution_part_two
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+         # self.assertEqual
+
+
+if __name__ == '__main__':
+    unittest.main()
