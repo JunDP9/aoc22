284caef05332c68f8d2b531ee96a0a29e716a4d3
Jun Chatani <jun.chatani@persgroep.net>
2022-12-07 08:28:18 +0100

    Day 6: Refactor

--- day6/__init__.py
+++ day6/__init__.py
@@ -12,10 +12,7 @@ def solution_part_one(parsed_string):
         if char in res:
             while char in res:
                 res.pop(0)
-            res.append(char)
-
-        else:
-            res.append(char)
+        res.append(char)
         counter += 1
         if len(res) > 3: return counter
     return counter
@@ -28,13 +25,11 @@ def solution_part_two(parsed_string):
         if char in res:
             while char in res:
                 res.pop(0)
-            res.append(char)
-
-        else:
-            res.append(char)
+        res.append(char)
         counter += 1
         if len(res) > 13: return counter
     return counter
+
 
 print(solution_part_one(parsed_string))
 print(solution_part_two(parsed_string))
