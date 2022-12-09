70d06f6c2c16d3a0fc0852a22576ec399cd77cb2
Jun Chatani <jun.chatani@persgroep.net>
2022-12-09 09:49:20 +0100

    Day 7: Extra comments

--- day7/__init__.py
+++ day7/__init__.py
@@ -8,6 +8,10 @@ parsed_input = parse_input(FILE_PATH)
 
 def solution_part_one():
     tree, dirs = get_file_structure()
+    # sum_sizes = 0
+    # for size in dirs.values():
+    #     if size <= 100000:
+    #         sum_sizes += size
     return sum(size for size in dirs.values() if size <= 100000)
 
 
@@ -33,6 +37,12 @@ def get_file_structure():
 def solution_part_two():
     tree, dirs = get_file_structure()
     required = 30000000 - (70000000 - dirs[("/",)])
+    # min_list = []
+    # for size in dirs.values():
+    #     if size >= required:
+    #         min_list.append(size)
+    # print(min(min_list))
+    # oneliner thank u reddit
     return min(size for size in dirs.values() if size >= required)
 
 
