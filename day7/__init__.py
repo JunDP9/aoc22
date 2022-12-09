0f83110391bad12cca6bde77ba8c73eba8336f6f
Jun Chatani <jun.chatani@persgroep.net>
2022-12-09 09:24:33 +0100

    Day 7: Cleanup and refactor,

--- day7/__init__.py
+++ day7/__init__.py
@@ -1,60 +1,42 @@
 from utils import parse_input
+from collections import defaultdict
 
-FILE_PATH = 'nput.txt'
+FILE_PATH = 'input.txt'
 
 parsed_input = parse_input(FILE_PATH)
 
 
-def solution_part_one(parsed_input):
-    res = []
-    counter = 0
-
-    tree = get_file_structure(parsed_input)
-    for key, value in tree.items():
+def solution_part_one():
+    tree, dirs = get_file_structure()
+    return sum(size for size in dirs.values() if size <= 100000)
 
 
-
-    print(tree)
-
-
-    return counter
-
-
-def get_file_structure(parsed_input):
-    tree = {"root": {}}
-    cur_dir = tree["root"]
-    prev_dir = [cur_dir]
+def get_file_structure():
+    path = []
+    dirs = defaultdict(int)
     for command in parsed_input:
         if command.startswith("$ cd"):
             [_, _, to_dir_name] = command.split(" ")
-            if to_dir_name.isalpha() or to_dir_name == "root":
-                prev_dir.append(cur_dir)
-                cur_dir = cur_dir[to_dir_name]
-            elif to_dir_name == "..":
-                cur_dir = prev_dir.pop()
+            if to_dir_name == "..":
+                path.pop()
+            else:
+                path.append(to_dir_name)
         elif command.startswith("$ ls"):
             continue
-        elif command.startswith("dir"):
-            [_, dir_name] = command.split(" ")
-            cur_dir[dir_name] = {}
-        else:
-            [size, file_name] = command.split(" ")
-            cur_dir[file_name] = int(size)
-    return tree
+        elif not command.startswith("dir"):
+            [size, _] = command.split(" ")
+            for i in range(len(path)):
+                dirs[tuple(path[: i + 1])] += int(size)
+    return path, dirs
 
 
-# def solution_part_two(parsed_string):
-#     res = []
-#     counter = 0
-#     for char in parsed_string[0]:
-#         if char in res:
-#             while char in res:
-#                 res.pop(0)
-#         res.append(char)
-#         counter += 1
-#         if len(res) > 13: return counter
-#     return counter
+def solution_part_two():
+    tree, dirs = get_file_structure()
+    required = 30000000 - (70000000 - dirs[("/",)])
+    return min(size for size in dirs.values() if size >= required)
 
 
-print(solution_part_one(parsed_input))
-# print(solution_part_two(parsed_input))
+print(solution_part_one())
+print(solution_part_two())
+# 488205 fout
+# 529468 fout
