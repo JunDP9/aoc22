7ce980468bfdba1280b2adf63e71966d34d27b55
Jun Chatani <jun.chatani@persgroep.net>
2022-12-10 14:26:51 +0100

    Day 9: Refactor wip

--- day10/__init__.py
+++ day10/__init__.py
@@ -6,7 +6,13 @@ parsed_input = parse_input(FILE_PATH)
 
 
 def solution_part_one(commands):
-    # suboptimal two iterations solution
+    stack = get_cycle_stack(commands)
+    specials = [20, 60, 100, 140, 180, 220]
+
+    return sum(sum(stack[:special]) * special for special in specials)
+
+
+def get_cycle_stack(commands):
     stack = [1]
     for command in commands:
         stack.append(0)
@@ -14,21 +20,43 @@ def solution_part_one(commands):
             pass
         else:
             stack.append(int(command.split(" ")[1]))
+    return stack
 
-    print(stack)
 
-    specials = [20,
-                60,
-                100,
-                140,
-                180,
-                220]
+def solution_part_two(commands):
+    stack = get_cycle_stack(commands)
 
-    total_sum = 0
-    for special in specials:
-        total_sum += sum(stack[:special]) * special
-    return total_sum
+    specials = [0, 40, 80, 120, 160, 200, 240]
+
+    res = []
+
+    sprite_left_index, sprite_middle_index, sprite_right_index = 0, 1, 2
+    for index in range(0, len(specials) - 1):
+        sprite_left_index, sprite_middle_index, sprite_right_index = generate_line(res, (stack[specials[index]:specials[index + 1]]), sprite_left_index, sprite_middle_index, sprite_right_index)
+    print(''.join(res[:40]))
+    print(''.join(res[40:80]))
+    print(''.join(res[80:120]))
+    print(''.join(res[120:160]))
+    print(''.join(res[160:200]))
+    print(''.join(res[200:240]))
+
+    return "none"
+
+
+def generate_line(res, split, sprite_left_index, sprite_middle_index, sprite_right_index):
+    for stack_index in range(0, len(split)):
+
+        if sprite_left_index == stack_index or sprite_middle_index == stack_index or sprite_right_index == stack_index:
+            res.append("#")
+        else:
+            res.append(" ")
+
+        sprite_middle_index += split[stack_index]
+        sprite_left_index = sprite_middle_index - 1
+        sprite_right_index = sprite_middle_index + 1
+
+    return sprite_left_index, sprite_middle_index, sprite_right_index
 
 
 print(solution_part_one(parsed_input))
-# print(solution_part_two(parsed_input))
+print(solution_part_two(parsed_input))
